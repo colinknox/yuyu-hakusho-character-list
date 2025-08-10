0d7bcba0ac133c0611b5ce36b9e7a1990d3c8fb9
colinknox <colinwknox@gmail.com>
2025-08-10 00:47:11 -0400

    added starter code

---
+++ main.py
@@ -0,0 +1,28 @@
+def main():
+    yyh_characters()
+
+def yyh_characters():
+    character_list = [
+        "yusuke urameshi",
+        "kazuma kawabara",
+        "kurama",
+        "hiei",
+        "genkai",
+        "botan",
+        "keiko yukimura",
+        "koenma",
+        "toguro",
+        "karasu",
+        "shinobu sensui",
+        "itsuki",
+        "suzaku",
+        "raizen",
+        "mukuro",
+        "yomi"
+    ]
+
+    print(character_list)
+
+
+
+main()
